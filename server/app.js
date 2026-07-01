8fbfda941df0379dc9614b18ff1fc5db3e679ef5
vilas-naik <113898490+vilas-naik@users.noreply.github.com>
2026-07-01 20:40:45 +0530

    Fixed routing issue

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://workchat-realtime.netlify.app/register",
+      "https://workchat-realtime.netlify.app",
     ],
     credentials: true,
   })
